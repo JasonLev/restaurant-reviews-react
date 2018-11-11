a65cc0d69303e32102ff2c81da2eb4bfce8f505a
JasonLev <jasond.levy@gmail.com>
2018-11-10 22:31:59 -0800

    f/star rating UI

--- src/components/Ratings.js
+++ src/components/Ratings.js
@@ -1,25 +1,23 @@
 import React from 'react'
 
 const Rating = (props) => (
-
-  <span className="mbl">
+  <label htmlFor={`star-${props.data}`} className={props.checked/20 >= props.data ? "fill" : "unfill"}>
     <input className="star"
            id={`star-${props.data}`}
            type="radio"
            name="star"
-           checked={props.checked}
+           checked={props.checked === 20}
            value={props.data}/>
-    <label htmlFor={`star-${props.data}`}>{props.data}</label>
-  </span>
+  </label>
 )
 
 const Ratings = (props) => {
   let ratings = []
-  ratings.push(<Rating key="rating-0" data={1} checked={props.defaultSelect === 20} />);
-  for (let i = 2; i < 6; i++) {
-    ratings.push(<Rating key={`rating-${i}`} data={i} />);
+  for (let i = 5; i > 1; i--) {
+    ratings.push(<Rating key={`rating-${i}`} data={i} checked={props.defaultSelect} />);
   }
-  return <div>{ratings}</div>
+  ratings.push(<Rating key="rating-0" data={1} checked={props.defaultSelect} />);
+  return <div className="rating">{ratings}</div>
 }
 
 export default Ratings
